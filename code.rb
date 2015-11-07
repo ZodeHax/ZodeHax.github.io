1aeac44b7b9cd2a0672c4aa44513aed38c86d749
ZodeHax <zode.hax@gmail.com>
2015-11-07 14:20:30 +0100

    Update code.rb

--- code.rb
+++ code.rb
@@ -2,4 +2,4 @@ maxnum=Kernel.pbMessageFreeText(_INTL("Enter a Number"),
                                  _INTL("1337"),
                                  false,6,Graphics.width)
 num=rand(maxnum)+1
-Kernel.pbMessage("#{num} / #{maxnum}")
+Kernel.pbMessage("#{num} / #{maxnum} - Edited")
